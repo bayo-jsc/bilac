9caf1ab26f01e08e9e76fda3ab3ccf5310159fb9
Anh Mai <theanht1k20@gmail.com>
2017-07-09 08:51:32 +0700

    New algorithm to schedule matches

--- controllers/tours_controller.go
+++ controllers/tours_controller.go
@@ -38,7 +38,6 @@ func CreateTournament(c *gin.Context) {
 	var request models.TeamRequest
 
 	c.BindJSON(&request)
-	teams_raw := request.Teams
 
 	tour := models.Tournament{}
 	db.Create(&tour)
@@ -46,47 +45,14 @@ func CreateTournament(c *gin.Context) {
 		c.JSON(500, gin.H{"error": err})
 	} else {
 		// Create teams
-		for i := 0; i < len(teams_raw); i++ {
-			team := models.Team {
-				TournamentID: tour.ID,
-				Member1ID: uint(teams_raw[i].Member1_id),
-				Member2ID: uint(teams_raw[i].Member2_id),
-			}
-			db.Create(&team)
-		}
+		var teams []models.Team
+		teams = tour.CreateTeams(request)
 
 		// Create matches
-		var teams []models.Team
-		db.Model(&tour).Related(&teams)
-		for i := 0; i < len(teams)-1; i++ {
-			for j := i + 1; j < len(teams); j++ {
-				match := models.Match {
-					TournamentID: tour.ID,
-					Team1ID: teams[i].ID,
-					Team2ID: teams[j].ID,
-				}
+		tour.CreateMatches(teams)
 
-				db.Create(&match)
-			}
-		}
-		// Shuffle Match
-		db.Model(tour).Related(&tour.Matches)
-		tour.ShuffleMatch()
 		c.JSON(201, tour)
 	}
-}
-
-func ShuffleMatch(c *gin.Context) {
-	db := models.InitDB()
-	defer db.Close()
-
-	tour_id := c.Params.ByName("id")
-	var tour models.Tournament
-	db.Find(&tour, tour_id)
-
-	tour.ShuffleMatch()
-
-	c.JSON(200, tour)
 }
 
 func UpdateMatchScore(c *gin.Context)  {
--- models/tournament.go
+++ models/tournament.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
+
+	//"fmt"
 )
 
 type Tournament struct {
@@ -11,19 +13,74 @@ type Tournament struct {
 	Teams []Team `gorm:"ForeignKey:TournamentID"`
 }
 
-func (tour *Tournament) ShuffleMatch() {
-  db := InitDB()
-  defer db.Close()
+func (tour *Tournament) CreateTeams(request TeamRequest) []Team {
+	db := InitDB()
+	defer db.Close()
 
-  var oldOrder []Match
-  db.Model(tour).Related(&oldOrder)
+	teamRaw := request.Teams
+	var teams []Team
+	for _, raw := range teamRaw {
+		team := Team {
+			TournamentID: tour.ID,
+			Member1ID: uint(raw.Member1_id),
+			Member2ID: uint(raw.Member2_id),
+		}
+		db.Create(&team)
+		teams = append(teams, team)
+	}
+	return teams
+}
 
-  db.Model(tour).Order("RANDOM()").Related(&tour.Matches)
+func (tour *Tournament) CreateMatches(teams []Team) []Match {
+	db := InitDB()
+	defer db.Close()
 
-  for i, _ := range tour.Matches {
-    tour.Matches[i].GetMatchInfo(oldOrder[i])
-  }
+	nTeam := len(teams)
+	// Add a dummy teams if number of teams is odd
+	if nTeam % 2 == 1 {
+		nTeam += 1
+	}
 
-  db.Save(&tour)
-  return
+	candidates := make([]int, nTeam)
+	for i := range candidates {
+		candidates[i] = i
+	}
+
+	var matches []Match
+	// For each round
+	for round := 1; round < nTeam; round++ {
+		// Team i in upper row
+		for i := 0; i < nTeam / 2; i++ {
+			// Team j in lower row
+			j := nTeam - i - 1
+
+			team1 := candidates[i]
+			team2 := candidates[j]
+			// Change Home/Away
+			if round % 2 == 0 {
+				team1, team2 = team2, team1
+			}
+
+			// Next if i or j is dummy team
+			if len(teams) % 2 == 1 && (team1 == nTeam - 1 || team2 == nTeam - 1) {
+				continue
+			}
+
+			match := Match {
+				TournamentID: tour.ID,
+				Team1ID: teams[team1].ID,
+				Team2ID: teams[team2].ID,
+			}
+
+			db.Create(&match)
+			matches = append(matches, match)
+		}
+
+		// Rotate candidate, follow the rule
+		// https://stackoverflow.com/questions/6648512/scheduling-algorithm-for-a-round-robin-tournament
+		tail := append(candidates[nTeam-1:], candidates[1:nTeam-1]...)
+		candidates = append(candidates[:1], tail...)
+	}
+
+	return matches
 }
